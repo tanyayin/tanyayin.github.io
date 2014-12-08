83fbbd2693811b6f0a7b469db3d51a81cb53b5d0
tanyayin <1156567532@qq.com>
2014-12-08 09:58:23 +0800

    2014.12.08

--- javascripts/tanya.js
+++ javascripts/tanya.js
@@ -49,9 +49,41 @@
 
 //---------------------------------------
 
-$(function(){
-	$('.art_aside>ul>li').click(function(){
-		$(this).find('.art_aside ul li ul').css('display','block');
-	})
-	
+// JavaScript Document
+var handler = null;
+//定义基本属性.
+var options = {
+    autoResize: true,
+    container: $('#main'),
+    offset: 2,
+    itemHeight: 210
+};
+
+//定义滚动函数
+function onScroll(event) {
+    //是否到底部（这里是判断离底部还有100px开始载入数据）.
+    var closeToBottom = ($(window).scrollTop() + $(window).height() > $(document).height() - 100);
+    if(closeToBottom) {
+        //这里就是AJAX载入的数据
+        $.ajax({url:"data.html", dataType:"html", success:function(html){
+            //把新数据追加到对象中
+            $('#list').append(html);
+            //清除原来的定位
+            if(handler) handler.wookmarkClear();
+            //创建新的wookmark对象
+            handler = $('#list dl');
+            handler.wookmark(options);
+            }
+        });
+    }
+};
+
+$(document).ready(new function() {
+    //绑定scroll事件.
+    $(document).bind('scroll', onScroll);
+    //第一次布局.
+    handler = $('#list dl');
+    handler.wookmark(options);
 });
+
+
